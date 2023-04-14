9bee404e9a597a8745f8d4078c3d2e3ca491282a
Drsamzz <112510651+Drsamzz@users.noreply.github.com>
2023-04-14 17:06:06 -0400

    Delete javaScript directory

--- javaScript/header.js
+++
@@ -1,7 +0,0 @@
-const options = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': '8466d65f13mshabd4cb07fb2a334p14f08fjsneba4e47c7682',
-        'X-RapidAPI-Host': 'moviesdatabase.p.rapidapi.com'
-    }
-};
